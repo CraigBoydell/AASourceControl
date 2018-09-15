5d02a04da75aae4e1324beac89ba7fe255508ca6
CraigBoydell <32689728+CraigBoydell@users.noreply.github.com>
2018-09-15 15:18:04 +1000

    Update Create-NSGs.ps1

--- Create-NSGs.ps1
+++ Create-NSGs.ps1
@@ -1,1 +1,1 @@
-New-AzureRmResourceGroupDeployment -ResourceGroupName SG-HomeLab-SCOM-RG-02 -TemplateFile $($PSScriptRoot + '\AzureRMTemplates\NSGsDeploy to SG-HomeLab-SCOM-RG-02.json') -DeploymentName NSGsDeploy -Force -DeploymentDebugLogLevel All -Mode Complete
+New-AzureRmResourceGroupDeployment -ResourceGroupName SG-HomeLab-SCOM-RG-02 -TemplateFile $($PSScriptRoot + '\AzureRMTemplates\NSGsDeploy to SG-HomeLab-SCOM-RG-02.json') -DeploymentName NSGsDeploy -Force -Mode Complete
