d9009442e6d8b96300e7c3cbd5f1d306135f7f39
CraigBoydell <32689728+CraigBoydell@users.noreply.github.com>
2018-09-15 13:32:10 +1000

    Update Create-ResourceGroups.ps1

--- Create-ResourceGroups.ps1
+++ Create-ResourceGroups.ps1
@@ -1,22 +1,14 @@
 #$CMDB = import-csv '\\fs0001nsg\users$\folderredirection\cboydell\Documents\INGDirect\88. Azure QRM\CMDB-ResourceGroups.csv'
 $CMDB = import-csv $($PSScriptRoot + '\CMDB-ResourceGroups.csv')
 $CMDB
-$PSScriptRoot
-$PSCommandPath
-get-childitem D:\ -recurse | select-object -property FullName
 #$i = 0
-#$CMDB | foreach {
+$CMDB | foreach {
 #	$i++
 #	write-progress -Activity $($_.Name + " : " + $i + "/" + $CMDB.Count) -PercentComplete $([math]::Round(($i / $CMDB.Count) * 100, 2))
-#	$RG = Get-AzureRMResourceGroup -Name $_.Name -erroraction SilentlyContinue
-#	if ($RG -eq $NULL) {
-#		New-AzureRMResourceGroup -Name $_.Name -Location $_.Location -Tags @{Description = $_.Description}
-#	} else {
-#		if ($RG.Tags['Description'] -ne $_.Description) { $RG.Tags.Description = $_.Description; $RG | Set-AzureRMResourceGroup }
-#	}	
-#}
-"Hello World v2"
-$VMs = Get-AzureRmVM
-$VMs[0].name
-get-AzureRmSubscription
-
+  $RG = Get-AzureRMResourceGroup -Name $_.Name -erroraction SilentlyContinue
+	if ($RG -eq $NULL) {
+		New-AzureRMResourceGroup -Name $_.Name -Location $_.Location -Tags @{Description = $_.Description}
+  } else {
+		if ($RG.Tags['Description'] -ne $_.Description) { $RG.Tags.Description = $_.Description; $RG | Set-AzureRMResourceGroup }
+	}	
+}
