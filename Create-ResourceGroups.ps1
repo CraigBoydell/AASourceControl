9026df6c5484b8f65f9471353f34d3cfa617b386
CraigBoydell <32689728+CraigBoydell@users.noreply.github.com>
2018-09-15 13:37:27 +1000

    Update Create-ResourceGroups.ps1

--- Create-ResourceGroups.ps1
+++ Create-ResourceGroups.ps1
@@ -7,7 +7,7 @@ $CMDB | foreach {
 #	write-progress -Activity $($_.Name + " : " + $i + "/" + $CMDB.Count) -PercentComplete $([math]::Round(($i / $CMDB.Count) * 100, 2))
   $RG = Get-AzureRMResourceGroup -Name $_.Name -erroraction SilentlyContinue
 	if ($RG -eq $NULL) {
-		New-AzureRMResourceGroup -Name $_.Name -Location $_.Location -Tags @{Description = $_.Description}
+		$NewRG = New-AzureRMResourceGroup -Name $_.Name -Location $_.Location -Tags @{Description = $_.Description}
   } else {
 		if ($RG.Tags['Description'] -ne $_.Description) { $RG.Tags.Description = $_.Description; $RG | Set-AzureRMResourceGroup }
 	}	
